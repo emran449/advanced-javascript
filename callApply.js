e4b938f5e865688646cb47182bc697e7498a4372
Emran <mdshojibkhan934@gmail.com>
2024-05-29 20:23:20 +0600

    gfgfd

--- callApply.js
+++ callApply.js
@@ -6,11 +6,35 @@ const normalPerson = {
         console.log(this.firstName, this.lastName);
     },
     chargeBill: function(amount){
+        console.log(this)
         this.salary = this.salary - amount;
         return this.salary;
     }
 }
 
-normalPerson.chargeBill(150);
-normalPerson.chargeBill(3000);
+// normalPerson.chargeBill(150);
+// normalPerson.chargeBill(3000);
+// console.log(normalPerson.salary);
+
+const heroPerson = {
+    firstName: "Hero",
+    lastName: 'Balam',
+    salary: 25000
+}
+
+const friendlyPerson = {
+    firstName: "Hero",
+    lastName: 'Kalam',
+    salary: 25000
+}
+
+// normalPerson.chargeBill();
+const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
+heroBillCharge(2000);
+heroBillCharge(3000);
+console.log(heroPerson.salary);
 console.log(normalPerson.salary);
+
+const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+friendlyChargeBill(1500);
+console.log(friendlyPerson.salary);
