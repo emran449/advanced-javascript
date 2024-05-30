285d1c171c9953590aac9e81424046c2e2444278
Emran <mdshojibkhan934@gmail.com>
2024-05-30 12:46:43 +0600

    ffgre

--- callApply.js
+++ callApply.js
@@ -5,9 +5,9 @@ const normalPerson = {
     getFullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount, tips, tax){
         console.log(this)
-        this.salary = this.salary - amount;
+        this.salary = this.salary - amount - tips - tax;
         return this.salary;
     }
 }
@@ -29,12 +29,23 @@ const friendlyPerson = {
 }
 
 // normalPerson.chargeBill();
-const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
-heroBillCharge(2000);
-heroBillCharge(3000);
-console.log(heroPerson.salary);
-console.log(normalPerson.salary);
+// const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
+// heroBillCharge(2000);
+// heroBillCharge(3000);
+// console.log(heroPerson.salary);
+// console.log(normalPerson.salary);
 
-const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
-friendlyChargeBill(1500);
-console.log(friendlyPerson.salary);
+// const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+// friendlyChargeBill(1500);
+// console.log(friendlyPerson.salary);
+
+// normalPerson.chargeBill.call(heroPerson, 900, 100, 10);
+// normalPerson.chargeBill.call(heroPerson, 3000, 300, 30);
+// console.log(heroPerson.salary);
+
+// normalPerson.chargeBill.call(friendlyPerson, 5000, 500, 50);
+// console.log(friendlyPerson.salary);
+// console.log(normalPerson.salary);
+
+ normalPerson.chargeBill.apply(heroPerson, [3000, 300, 30]);
+console.log(heroPerson.salary);
